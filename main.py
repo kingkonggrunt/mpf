2782ad98436bf0f9f84975eb545728d48ac2715a
kingkonggrunt <kingkonggrunt@users.noreply.github.com>
2022-01-10 23:08:12 +1100

    feat: final commit

--- main.py
+++ main.py
@@ -1,17 +1,43 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import chrome, edge
 
 edge_driver = "drivers/msedgedriver.exe"
 url = 'https://www.bing.com'
 
+
 if __name__ == '__main__':
-    driver = webdriver.Edge(edge_driver)
+    edge_options = edge.options.Options()
+    edge_options._binary_location = "C:/Program Files (x86)/Microsoft\Edge/Application/msedge.exe"
+    edge_options.add_argument('--disable-blink-features=AutomationControlled')  # thanks @soumil-shah
+    edge_options.add_argument('--disable-blink-features=AutomationControlled')  # thanks @soumil-shah
+    edge_options.add_experimental_option("useAutomationExtension", False)
+    edge_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+
+    driver = webdriver.Edge(executable_path=edge_driver, options=edge_options)
+    driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")  # thanks @soumil-shah
+    driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+            "source":
+                "const newProto = navigator.__proto__;"
+                "delete newProto.webdriver;"
+                "navigator.__proto__ = newProto;"
+        })  # thanks @soumil-shah
+
     driver.get(url)
 
-    elm_search_box = driver.find_element(value="sb_form_q")
-    elm_search_box.send_keys("some keys")
-    elm_search_box.send_keys(Keys.ENTER)
+    from data.searches import common_search_terms
+    from random import uniform, choice
+    from time import sleep
 
-    elm_b_search_box = driver.find_element(value="sb_form_q")
-    elm_b_search_box.clear()
-    elm_b_search_box.send_keys("testing")
+
+    for i in range(30):
+        sleep(uniform(1.7, 4))
+        elm_search_box = driver.find_element(value="sb_form_q")
+        elm_search_box.clear()
+        sleep(uniform(0.5, 2))
+        elm_search_box.send_keys(choice(common_search_terms))
+        sleep(uniform(0.3, 0.9))
+        elm_search_box.send_keys(Keys.ENTER)
+        sleep(uniform(6, 10.5))
+
+    driver.quit()
